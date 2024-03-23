630251d238ee88b20154471de398a393061d378b
matheusinhoxuxu <matheusr.monte2004@gmail.com>
2024-03-22 21:30:07 -0300

    métodos HTTPs

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,34 @@
 const express = require('express')
+const Produto = require('../models/produto')
 const router = express.Router()
 
 router.get('/', function(req,res){
     res.json({})
 })
+
+router.get('/produtos', async function(req,res){
+    res.json( await Produto.find() )
+})
+
+router.post('/produtos', async function(req,res){ // o browser só aceita o método GET, mas nem um outro 
+    res.json( await Produto.create(req.body) )
+
+})
+
+router.get('/produtos/:id', async function(req,res){ // o browser só aceita o método GET, mas nem um outro 
+    res.json( await Produto.findById(req.params.id) )
+
+})
+
+router.delete('/produtos/:id', async function(req,res){ // o browser só aceita o método GET, mas nem um outro 
+    res.json( await Produto.findByIdAndDelete(req.params.id) )
+
+})
+
+router.put('/produtos/:id', async function(req,res){ // o browser só aceita o método GET, mas nem um outro 
+    res.json( await Produto.findByIdAndUpdate(req.params.id, req.body) )
+})
+
+
 
 module.exports = router
